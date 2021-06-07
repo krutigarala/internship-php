404f9d15cba98b78b7ca56978a0337520aa5ee1b
kruti06 <krutigarala01@gmail.com>
2021-06-07 23:12:17 +0530

    day 4 task
    modicare project

---
+++ day4/Modicare_Project/home.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <?php 
+    include './title.php';
+    ?>
+</head>
+<body>
+<div id="header">
+	<ul>
+    <li class="current"><a href="home.php"><span>Home</span></a></li>
+    <li><a href="products.php"><span>Products</span></a></li>
+    <li><a href="videos.php"><span>Videos</span></a></li>
+    <li><a href="gallery.php"><span>Gallery</span></a></li>
+    <li><a href="about.php"><span>About Us</span></a></li>
+    <li><a href="contact.php"><span>Contact Us</span></a></li>
+	</ul>
+    <span id="background"></span>
+    <div id="home">
+    <?php 
+        include './section.php';
+    ?>
+    <div id="featured">
+    <img src="images/yoga-girl.gif"/>
+    <ul><li>
+    <a href="classes.php"><img src="images/modicare_vpart1.jpg" alt="Image" /></a>
+    <div><a href="classes.php">Products</a></div>
+    </li>
+    <li>
+        <a href="videos.php"><img src="images/modicare_video.jpg" alt="Image" /></a>
+    <div><a href="videos.php">Videos</a></div>
+    </li></ul>
+    </div>
+    </div>
+    </div>
+    <div id="content">
+    <ul class="first">
+    <li class="first">
+    <h3><a href="home.php">Welcome to <span>Modicare</span></a></h3>
+    <p>Our story led by leaders, innovators, philanthropists and entrepreneurs who cared deeply about giving the common man a platform where he could have freedom of dignity, freedom of choice and the biggest dream of all- financial freedom.</p>
+    </li>
+    <li>
+    <h3><a href="home.php">Our <span>Story</span></a></h3>
+    <p>Our story is one of innovation, of dreaming beyond the ordinary, of caring for the common man which would translate into creating a business empire whose greatest wealth were its people, whose greatest success was its passion, and whose only philosophy- dare to dream and make those dreams come alive.</p>
+    </li></ul>
+    </div>
+    <?php 
+        include './footer.php';
+    ?>
+</body>
+</html>
